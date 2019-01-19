07dbb6d60cab7716c00882d26bb53fad377ee05c
ZFMing <821511832@qq.com>
2019-01-20 00:59:46 +0800

    Add R.code

---
+++ 简单计算.R
@@ -0,0 +1,43 @@
+# R的基本用途1：大型计算器
+# 基本运算符号：+, -, *, /, ^
+# 高级运算符号： log, exp, sin, cos, tan, sqrt, etc.
+# 特殊符号：max(), min(), sum(), etc
+# 其他诸如向量、矩阵、数组等对象的运算在后面讲述。
+#
+# Yuehan Yang. 2014/7/18 yyh@cufe.edu.cn
+
+
+
+
+2+2 # addition
+#
+3-1 # substraction
+#
+2*5 # multiplication
+#
+10/2 # division
+#
+10^2 # power
+#
+100^(1/2) # square root
+#
+sin(pi/2) # sin
+#
+exp(1) # exponential
+#
+log(exp(2)) # log, default base is e
+#
+log(10^3,base=10) # you can also specify the base
+#
+((10+2)*5-2^4)/4
+#
+sqrt(3)+exp(-2)
+#
+
+
+# 小技巧之退出命令
+# 1. 快捷键 Ctrl+q
+# 2. 程序运行窗口中输入：
+# > q()
+# 关闭 Rgui 或 RStudio
+
